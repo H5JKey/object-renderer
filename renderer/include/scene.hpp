6e013d41ddf7f87d375b0aeeda8336a860beb623
Ivan Belousov <ivanbelousoff96@gmail.com>
2026-07-09 20:43:51 +0300

    feat(renderer): implement basic ray casting for triangles

--- renderer/include/scene.hpp
+++ renderer/include/scene.hpp
@@ -1,5 +1,26 @@
 #pragma once
+#include <vector>
+
+struct vec3 {
+    vec3(float x, float y, float z) {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+    float x, y, z;
+    float padding;
+};
 
 class Scene {
-    
+public:
+    std::vector<int> indices;
+    std::vector<vec3> vertices;
+
+    vec3 origin;
+    vec3 lookAt;
+    vec3 backgroundColor;
+    vec3 sunColor;
+    vec3 sunDirection;
+public:
+    Scene();
 };
