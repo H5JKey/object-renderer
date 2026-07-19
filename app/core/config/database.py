ee83f9d5995aac5bec72c5540b25ae9a901a2c87
Nikolay Shirokov <shirkol2006@mail.ru>
2026-07-19 17:28:27 +0300

    feat(user): add users table to database

--- app/core/config/database.py
+++ app/core/config/database.py
@@ -14,6 +14,14 @@ class DataBaseConfig(BaseModel):
     pool_recycle: int = 3600
     echo: bool = False
 
+    naming_convention: dict[str, str] = {
+        "pk": "pk_%(table_name)s_%(column_0_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "ix": "ix_%(table_name)s_%(column_0_name)s",
+    }
+
     @property
     def url(self) -> str:
         return f"postgresql+asyncpg://{self.username}:{self.password}@{self.host}:{self.port}/{self.db_name}"
--- app/core/database.py
+++ app/core/database.py
@@ -1,15 +1,22 @@
 from collections.abc import AsyncGenerator
 
+from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
 from core.config.application import settings
+
+metadata = MetaData(
+    naming_convention=settings.database.naming_convention,
+)
 
 
 class Base(DeclarativeBase):
     """
     Базовый класс для наследования моделей базы данных.
     """
+
+    metadata: MetaData = metadata
 
 
 engine = create_async_engine(
