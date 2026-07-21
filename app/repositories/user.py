71559e1419b50b5735ee636a72e4f3785416a99f
Nikolay Shirokov <shirkol2006@mail.ru>
2026-07-21 17:41:56 +0300

    feat(user repository): add user repository interface and implementation

---
+++ app/repositories/user.py
@@ -0,0 +1,29 @@
+from sqlalchemy import delete
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.interfaces import AbstractUserRepository
+from models import User
+from schemas.user import UserCreate
+
+
+class UserRepository(AbstractUserRepository):
+    def __init__(self, session: AsyncSession) -> None:
+        self.session = session
+
+    async def get_by_id(self, user_id: int) -> User | None:
+        return await self.session.get(
+            entity=User,
+            ident=user_id,
+        )
+
+    async def create_user(self, create_user_data: UserCreate) -> User:
+        user = User(**create_user_data.model_dump())
+        self.session.add(user)
+        await self.session.commit()
+        await self.session.refresh(user)
+        return user
+
+    async def delete_by_id(self, user_id: int) -> None:
+        stmt = delete(User).where(User.id == user_id)
+        await self.session.execute(stmt)
+        await self.session.commit()
