5685bfb7c69c6e8e7a557ca033df22d170de7050
Ivan Belousov <ivanbelousoff96@gmail.com>
2026-07-22 16:37:03 +0300

    fix(renderer): replace eglGetDisplay with eglGetPlatformDisplay for headless mod

--- renderer/include/bvh.hpp
+++ renderer/include/bvh.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include <glm/glm.hpp>
-#include <limits>
 #include <vector>
 
 class BVH {
--- renderer/include/render-target.hpp
+++ renderer/include/render-target.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#define EGL_EGLEXT_PROTOTYPES
 #include <EGL/egl.h>
+#include <EGL/eglext.h>
 #include <glad/gl.h>
 
 #include <stdexcept>
