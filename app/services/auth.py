4f0996bbeb42c021e4430565a0921719c8734b6c
Nikolay Shirokov <shirkol2006@mail.ru>
2026-07-24 22:51:25 +0300

    feat(auth service): add validation data to auth service methods

--- app/services/auth.py
+++ app/services/auth.py
@@ -1,4 +1,11 @@
 from core.constants import BEARER_TOKEN_TYPE
+from core.exceptions.auth import InvalidPasswordError
+from core.exceptions.user import (
+    UserEmailAlreadyExistsError,
+    UserIdNotFoundError,
+    UserUsernameAlreadyExistsError,
+    UserUsernameNotFoundError,
+)
 from core.interfaces.repositories import AbstractUserRepository
 from core.interfaces.services import AbstractAuthService
 from core.security.jwt_tokens.factory import create_access_token, create_token
@@ -9,19 +16,23 @@ from core.security.password_utils import (
 from schemas.auth import LoginRequest, RegisterRequest
 from schemas.token import TokenInfo
 from schemas.user import UserResponse
-from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class AuthService(AbstractAuthService):
     def __init__(
         self,
-        session: AsyncSession,
         user_repository: AbstractUserRepository,
     ) -> None:
-        self.session = session
         self.user_repository = user_repository
 
     async def register(self, register_user_data: RegisterRequest) -> TokenInfo:
+        user = await self.user_repository.get_by_username(register_user_data.username)
+        if user is not None:
+            raise UserUsernameAlreadyExistsError(register_user_data.username)
+        user = await self.user_repository.get_by_email(register_user_data.email)
+        if user is not None:
+            raise UserEmailAlreadyExistsError(register_user_data.email)
+
         create_user_data = convert_register_to_create_user(register_user_data)
         user = await self.user_repository.create_user(create_user_data)
         user_response = UserResponse.model_validate(user)
@@ -34,14 +45,15 @@ class AuthService(AbstractAuthService):
     ) -> TokenInfo:
         user = await self.user_repository.get_by_username(auth_user_data.username)
         if user is None:
-            raise NotImplementedError
+            raise UserUsernameNotFoundError(auth_user_data.username)
+
         password = auth_user_data.password
         encrypted_password = user.encrypted_password
         if not validate_password(
             password=password,
             encrypted_password=encrypted_password,
         ):
-            raise NotImplementedError
+            raise InvalidPasswordError(password)
 
         user_response = UserResponse.model_validate(user)
         token = create_token(user_response)
@@ -49,6 +61,9 @@ class AuthService(AbstractAuthService):
 
     async def refresh_access_token(self, user_id: int) -> TokenInfo:
         user = await self.user_repository.get_by_id(user_id)
+        if user is None:
+            raise UserIdNotFoundError(user_id)
+
         user_response = UserResponse.model_validate(user)
         access_token = create_access_token(user_response)
         token = TokenInfo(
