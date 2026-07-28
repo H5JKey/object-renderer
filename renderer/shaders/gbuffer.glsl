c674788399c4bd6f9f2712633c874e23af9d718f
Ivan Belousov <ivanbelousoff96@gmail.com>
2026-07-29 01:00:10 +0300

    fix(renderer): prevent writing uninitialized normal and albedo to normalMap

--- renderer/shaders/gbuffer.glsl
+++ renderer/shaders/gbuffer.glsl
@@ -260,17 +260,19 @@ void main() {
     HitInfo hit;
     if (bvhNodes.length() > 0) {
         hit = castRayThroughBVH(uOrigin, direction);
-        imageStore(normalMap, pixel, vec4(hit.normal, 1.0));
     }
     else {
         imageStore(normalMap, pixel, vec4(0.0,0.0,0.0, 1.0));
-        hit.distance = MAX_DIST;
+        imageStore(albedoMap, pixel, vec4(0.0,0.0,0.0, 1.0));
+        return;
     }
 
     if (hit.distance > MAX_DIST - 1) {
+        imageStore(normalMap, pixel, vec4(0,0,0, 1.0));
         imageStore(albedoMap, pixel, vec4(0,0,0, 1.0));
     }
     else {
+        imageStore(normalMap, pixel, vec4(hit.normal , 1.0));
         imageStore(albedoMap, pixel, vec4(getAlbedo(materials[hit.material_id],hit.texCoord) , 1.0));
     }
     
