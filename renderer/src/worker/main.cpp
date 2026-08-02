b0602dfa2a90a2531569458e1d51c5fde2cdf4f6
H5Jkey <ivanbelousoff@gmail.com>
2026-08-03 01:07:45 +0300

    feat(renderer-worker): add json parser

--- renderer/src/worker/main.cpp
+++ renderer/src/worker/main.cpp
@@ -1,8 +1,13 @@
 #include <cstdlib>
 #include <exception>
+#include <nlohmann/json.hpp>
 
 #include "kafka-consumer.hpp"
 #include "logger.hpp"
+
+using json = nlohmann::json;
+
+void renderPipeline(int width, int height, int samples) { /* Some render */ }
 
 int main() try {
     Logger::getInstance().debug = true;
@@ -12,6 +17,20 @@ int main() try {
     KafkaConsumer consumer("127.0.0.1:9093", "renderer_worker", "create_project");
     while (true) {
         std::string message = consumer.consume();
+        int width, height, samples;
+        try {
+            json json = json::parse(message);
+            width = json["width"];
+            height = json["height"];
+            samples = json["samples"];
+        } catch (const std::exception& e) {
+            Logger::getInstance().log(std::format("Failed to create or parse json from string: {}", message),
+                                      Logger::Level::ERROR);
+        }
+        Logger::getInstance().log(
+            std::format("Get data from Json: [width: {}  height: {}  samples: {}]", width, height, samples),
+            Logger::Level::DEBUG);
+        renderPipeline(width, height, samples);
     }
     Logger::getInstance().log("Renderer application stopped successfully", Logger::Level::INFO);
     return EXIT_SUCCESS;
