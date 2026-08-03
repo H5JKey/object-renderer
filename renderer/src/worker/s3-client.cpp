5b3a5955a92fa58deb7ae9582ad95a0258a8101a
H5JKey <ivanbelousoff@gmail.com>
2026-08-03 22:49:50 +0300

    feat(renderer-worker): add reading scene file from s3

---
+++ renderer/src/worker/s3-client.cpp
@@ -0,0 +1,45 @@
+#include "s3-client.hpp"
+
+#include <aws/core/Aws.h>
+
+#include <exception>
+#include <print>
+#include <stdexcept>
+
+#include "logger.hpp"
+
+S3Client::S3Client(std::string_view endpoint, Aws::Auth::AWSCredentials credentials) {
+    try {
+        Aws::Client::ClientConfiguration clientConfig;
+        clientConfig.endpointOverride = endpoint;
+        clientConfig.scheme = Aws::Http::Scheme::HTTP;
+        clientConfig.verifySSL = false;
+
+        s3Client = Aws::S3::S3Client(credentials, clientConfig,
+                                     Aws::Client::AWSAuthV4Signer::PayloadSigningPolicy::Never, false);
+    } catch (const std::exception& e) {
+        throw std::runtime_error(std::format("Failed to create S3 client: endpoint = {}", endpoint));
+    }
+    Logger::getInstance().log(std::format("S3 client was created: endpoint = {}", endpoint), Logger::Level::DEBUG);
+}
+
+std::vector<uint8_t> S3Client::getData(const Aws::String& bucketName, const Aws::String& objectKey) {
+    Aws::S3::Model::GetObjectRequest request;
+    request.SetBucket(bucketName);
+    request.SetKey(objectKey);
+
+    auto outcome = s3Client.GetObject(request);
+
+    if (outcome.IsSuccess()) {
+        Aws::IOStream& ioStream = outcome.GetResult().GetBody();
+        Logger::getInstance().log(std::format("Get data from S3 with bucket {} and key {}", bucketName, objectKey),
+                                  Logger::Level::INFO);
+        std::vector<uint8_t> data{std::istreambuf_iterator<char>(ioStream), std::istreambuf_iterator<char>()};
+
+        return data;
+    } else {
+        throw std::runtime_error(std::format("Failed to read from S3: ", outcome.GetError().GetMessage()));
+    }
+}
+
+S3Client::~S3Client() {}
